7745efd584aa5819b3686f8f464f965c50c6222a
haneenm0hamed1 <146862066+haneenm0hamed1@users.noreply.github.com>
2023-11-01 16:33:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,74 @@
+const chatInput = document.querySelector(".chat-input textarea")
+const sendChatBtn = document.querySelector(".chat-input span")
+const chatbox = document.querySelector(".chatbox")
+const chatbotToggler =document.querySelector(".chatbot-toggler")
+const chatBotCloseBtn =document.querySelector(".close-btn")
+
+
+let userMessage
+const API_KEY = "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";
+
+
+const createChatLi = ( message, className) => {
+    const chatLi =document.createElement("li")
+    chatLi.classList.add("chat", className)
+    let chatContent = className === "outgoing" ? `<p>${message}</p>`: `<span class="material-symbols-outlined">smart_toy</span><p>${message}</p>`
+    chatLi.innerHTML = chatContent
+    return chatLi
+}
+const generateResponse = (incomingChatLi) => {
+   const API_URL = "https://api.openai.com/v1/chat/completions";
+const messageElement = incomingChatLi.querySelector("p")
+
+   const reustOptions = {
+    method: "POST",
+    headers: {
+        "Content-Type":"application/json",
+        "Authorization": `Bearer ${API_KEY}`
+    },
+    body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        "messages": [ {  role: "user",  content: userMessage }]
+    })
+
+   }
+   
+   fetch(API_URL,reustOptions).then(res => res.json()).then(data => {
+    messageElement.textContent = data.choices[0].message.content;
+   }).catch((error) => {
+    messageElement.textContent = "Oops! Something Went Wrong. Please Try Again "
+   }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+    }
+
+const handleChat = () => {
+    userMessage = chatInput.value.trim()
+    if( !userMessage) return;
+chatInput.value ="";
+
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight)
+
+    setTimeout (() => {
+        const incomingChatLi = createChatLi("Thinking...","incoming")
+        chatbox.appendChild(incomingChatLi);
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    generateResponse(incomingChatLi);
+}, 600)
+}
+
+chatInput.addEventListener("keydown", (e) =>{
+if(e.key === "Enter" && !e.shiftkey && window.innerWidth > 800 ) {
+    e.preventDefault();
+    handleChat();
+}
+}
+
+)
+
+
+sendChatBtn.addEventListener("click" , handleChat);
+chatBotCloseBtn.addEventListener("click" , () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click" , () => document.body.classList.toggle("show-chatbot"));
+
+
+ 
